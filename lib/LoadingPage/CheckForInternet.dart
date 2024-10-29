4c0fbd2932c20d72c648d0887fd9a8d97c2a5acb
jakubbaldyga <kubszcz@gmail.com>
2024-10-29 22:21:03 +0100

    loaing screen + check for internet

---
+++ lib/LoadingPage/CheckForInternet.dart
@@ -0,0 +1,16 @@
+
+import 'dart:io';
+
+class CheckForInternet {
+  static Future<bool> check() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        return true;
+      }
+    } on SocketException catch (_) {
+      return false;
+    }
+    return false;
+  }
+}
--- lib/LoadingPage/LoadingPage.dart
+++ lib/LoadingPage/LoadingPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:solvro_cocktails/LoadingPage/CheckForInternet.dart';
 import 'package:solvro_cocktails/Services/DataManagerSingleton.dart';
 import 'package:solvro_cocktails/Services/QueryOptions.dart';
 
@@ -22,6 +24,29 @@ class _LoadingPageState extends State<LoadingPage> {
   }
 
   void waitAndNavigate() async {
+
+    if(!await CheckForInternet.check()) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Connection Error'),
+            content: Text('No internet connection. Please check your settings.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close the dialog
+                  SystemNavigator.pop(); // Exit the app// Close the dialog
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
     while (DataManagerSingleton.getInstance().isBusy()) {
       await Future.delayed(Duration(milliseconds: 1000));
     }
@@ -35,7 +60,35 @@ class _LoadingPageState extends State<LoadingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(text),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image(
+                image: AssetImage('assets/loading.png'),
+                fit: BoxFit.cover
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child:
+                  Text(
+                    text,
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black
+                    )
+                  ),
+              )
+            ),
+          ]
+        ),
       ),
 
     );
